49c0900fde904f874450a0e5bc4c351444f04aaf
周涛 <zhoutaoo@foxmail.com>
2025-12-27 16:14:43 +0800

    增加开关是否开启，网关新增url权限校验功能，默认不开启url校验校验

--- src/main/java/io/github/opensabre/gateway/manager/DynamicAuthorizationManager.java
+++ src/main/java/io/github/opensabre/gateway/manager/DynamicAuthorizationManager.java
@@ -3,6 +3,7 @@ package io.github.opensabre.gateway.manager;
 import io.github.opensabre.gateway.entity.Authority;
 import io.github.opensabre.gateway.service.IAuthorityService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.authorization.AuthorizationDecision;
@@ -14,7 +15,6 @@ import org.springframework.security.web.server.util.matcher.ServerWebExchangeMat
 import org.springframework.stereotype.Component;
 
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Set;
@@ -27,13 +27,17 @@ public class DynamicAuthorizationManager implements ReactiveAuthorizationManager
 
     // 无权限
     public static final AuthorizationDecision AUTHORIZATION_DECISION_FALSE = new AuthorizationDecision(false);
-    private static final AuthorizationDecision AUTHORIZATION_DECISION_TRUE = new AuthorizationDecision(true);
+    // 有权限
+    public static final AuthorizationDecision AUTHORIZATION_DECISION_TRUE = new AuthorizationDecision(true);
 
     @Resource
     private IAuthorityService authorityService;
 
+    @Value("${opensabre.gateway.permission.enabled:false}")
+    private boolean permission;
+
     /**
-     * url级权限校验
+     * url 级权限校验
      *
      * @param authentication 认证信息
      * @param context        请求上下文
@@ -42,37 +46,39 @@ public class DynamicAuthorizationManager implements ReactiveAuthorizationManager
     @Override
     public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext context) {
         ServerWebExchange exchange = context.getExchange();
+        // 如果权限开关关闭，则直接放行
         // 如果是预检请求（OPTIONS），直接放行
-        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+        if (!permission || exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
             return Mono.just(AUTHORIZATION_DECISION_TRUE);
         }
         // 用户角色拥有的authorities 与 用户请求url所需authorities 进行匹配，任意包含则返回true(有权限)
         return authentication
+                // 认证通过的
                 .filter(Authentication::isAuthenticated)
-                // 用户token中的authorities 与 请求需要的authorities进行匹配，任意包含则通过返回 有权限
-                .flatMap(authToken -> {
-                    // 根据请求和用户角色拥有权限 获取匹配的 Authority列表
-                    return getAuthoritiesForRoles(authToken)
-                            // 通过 path pattern进行匹配，如果匹配成功，则表明有权限，返回true
-                            .filterWhen(authority -> match(authority, exchange))
-                            .hasElements()
-                            .map(AuthorizationDecision::new);
-                })
-                // 如果为空则返回false无权限
+                // 用户token中的角色 所拥有的 authorities 和请求进行匹配
+                .flatMap(authToken -> hasPermission(authToken, exchange))
+                // 如果为空则返回 false 无权限
                 .defaultIfEmpty(AUTHORIZATION_DECISION_FALSE);
     }
 
     /**
-     * 从token中获取角色，根据角色code加载权限集合(用户拥有的权限集合)
+     * 用户token中的authorities 与 请求需要的authorities进行匹配，任意包含则通过返回 有权限
      *
-     * @param authToken 用户token
-     * @return Set<Authority> 权限集合
+     * @param authToken 用户 token
+     * @param exchange  请求信息
+     * @return Mono<AuthorizationDecision> 是否匹配
      */
-    private Flux<Authority> getAuthoritiesForRoles(Authentication authToken) {
-        // 用户拥有的角色集合
+    private Mono<AuthorizationDecision> hasPermission(Authentication authToken, ServerWebExchange exchange) {
+        // 用户拥有的角色集合，从token中获取角色列表
         Set<String> roles = AuthorityUtils.authorityListToSet(authToken.getAuthorities());
-        // 用户拥有的角色 对应的 authorities集合
-        return authorityService.getAuthorityForRoles(roles);
+        // 根据请求和用户角色拥有权限 获取匹配的 Authority列表
+        // 用户拥有的角色 对应的 authorities集合，根据角色code加载权限集合(用户拥有的权限集合)
+        return authorityService.getAuthorityForRoles(roles)
+                // 通过 path pattern进行匹配，如果匹配成功，则表明有权限，返回true
+                .filterWhen(authority -> match(authority, exchange))
+                .hasElements()
+                // 匹配为true的，返回对应 AuthorizationDecision
+                .map(AuthorizationDecision::new);
     }
 
     /**
