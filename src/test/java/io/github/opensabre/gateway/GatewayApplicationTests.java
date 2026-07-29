6876ab01116be3b4526bb13bcba3f2567a31e4f1
zhoutaoo <zhoutaoo@foxmail.com>
2026-07-30 00:41:48 +0800

    ci: 镜像发布前执行测试并升级 Actions (#5)
    * ci: 镜像发布前执行测试并升级 Actions
    
    * ci: 隔离测试依赖并稳定测试门禁
    
    * test: 补全无认证请求的会话桩
    
    * test: 隔离 OIDC 元数据外部依赖
    
    * test: 隔离 OAuth2 客户端元数据仓库

--- src/test/java/io/github/opensabre/gateway/GatewayApplicationTests.java
+++ src/test/java/io/github/opensabre/gateway/GatewayApplicationTests.java
@@ -2,14 +2,23 @@ package io.github.opensabre.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @SpringBootTest(properties = {
         "spring.cloud.nacos.discovery.enabled=false",
         "spring.cloud.nacos.config.enabled=false",
         "spring.cloud.discovery.enabled=false",
-        "spring.cloud.gateway.discovery.locator.enabled=false"
+        "spring.cloud.gateway.discovery.locator.enabled=false",
+        "spring.security.oauth2.client.provider.custom-issuer.issuer-uri=",
+        "spring.security.oauth2.client.provider.custom-issuer.authorization-uri=http://localhost/oauth2/authorize",
+        "spring.security.oauth2.client.provider.custom-issuer.token-uri=http://localhost/oauth2/token",
+        "spring.security.oauth2.client.provider.custom-issuer.jwk-set-uri=http://localhost/oauth2/jwks"
 })
 public class GatewayApplicationTests {
+
+    @MockBean
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     public void contextLoads() {
