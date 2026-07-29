c62ed09c8757886d932a60247765e0bae3db39bc
周涛 <zhoutaoo@foxmail.com>
2026-07-30 00:33:09 +0800

    test: 隔离 OIDC 元数据外部依赖

--- src/test/java/io/github/opensabre/gateway/GatewayApplicationTests.java
+++ src/test/java/io/github/opensabre/gateway/GatewayApplicationTests.java
@@ -7,7 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
         "spring.cloud.nacos.discovery.enabled=false",
         "spring.cloud.nacos.config.enabled=false",
         "spring.cloud.discovery.enabled=false",
-        "spring.cloud.gateway.discovery.locator.enabled=false"
+        "spring.cloud.gateway.discovery.locator.enabled=false",
+        "spring.security.oauth2.client.provider.custom-issuer.issuer-uri=",
+        "spring.security.oauth2.client.provider.custom-issuer.authorization-uri=http://localhost/oauth2/authorize",
+        "spring.security.oauth2.client.provider.custom-issuer.token-uri=http://localhost/oauth2/token",
+        "spring.security.oauth2.client.provider.custom-issuer.jwk-set-uri=http://localhost/oauth2/jwks"
 })
 public class GatewayApplicationTests {
 
